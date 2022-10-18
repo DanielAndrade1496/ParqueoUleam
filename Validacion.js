fa49ad171669423858035681a7a946b48acf83c2
DanielAndrade1496 <92600040+DanielAndrade1496@users.noreply.github.com>
2022-10-18 14:05:25 -0500

    Validacion

---
+++ Validacion.js
@@ -0,0 +1,27 @@
+function Validar(user,pwd)
+		  {
+		   user=document.getElementById(user);
+		   pwd=document.getElementById(pwd);
+		   if(user.value=="")
+			{
+			 alert("El campo Nombre esta vacio");
+			 user.focus();
+			 return false;
+			}
+		   else
+			{
+			 if(pwd.value=="")
+			  {
+			   alert("El campo Passwd esta vacio");
+			   pwd.focus();
+			   return false;
+			  }
+			 else
+			  {
+			   var usuario = document.formulario.user.value;
+			   var password = document.formulario.password.value;
+			   window.location='procesoChecador.php?usuario='+usuario+'&pw='+password;
+			   return true;
+			  }
+			}
+		  }
