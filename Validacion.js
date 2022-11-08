1a4ef916228628071544a239d9840db2981e15f2
DanielAndrade1496 <92600040+DanielAndrade1496@users.noreply.github.com>
2022-11-08 14:26:55 -0500

    act

--- Validacion.js
+++ Validacion.js
@@ -1,21 +1,20 @@
 
-
-function Aprov(){
-    var Email= document.getElementById("Correo").value;
-    var password= document.getElementById("password").value;
-    var asc= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-    var validar=asc.test(Email);
+function Inicio(){
+    var Correo= document.getElementById("e1").value;
+    var contraseña= document.getElementById("c1").value;
+    var valico= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var validar=valico.test(Correo);
     if(validar == true){
-        if(password ==null || password.length == 0){
+        if(contraseña ==null || contraseña.length == 0){
             alert("Se debe llenar el campo contraseña")
         }
         else{
-            
+            document.getElementById('ini1').href='InicioMuerto.html'
         }
     }else{
         alert("Correo electronico no valido")
     }
     
-    
 }
+
 
